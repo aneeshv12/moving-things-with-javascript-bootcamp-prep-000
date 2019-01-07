b06e794e6689615db0b0b8247a98454ad087a8e2
Aneesh Venuturumilli <veeaneesh@gmail.com>
2019-01-07 20:51:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener('keydown',function(e){
 })
 function moveDodgerRight(){
   var leftNumbers = dodger.style.left.replace('px','');
-  var left = parseInt(rightNumbers,10);
+  var left = parseInt(leftNumbers,10);
   if(left>0){
     dodger.style.left = `${left+1}px`
   }}
